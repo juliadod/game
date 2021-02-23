4bec7ee228079bbb3288a696f71bc6dae55a36cd
juliadod <juliacloumic@gmail.com>
2021-02-23 21:45:47 +0400

    Создано начальное окно и добавлен текст выбора уровня

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+
+size = [1280, 720]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Парковка")
+
+clock = pygame.time.Clock()
+
+text = pygame.image.load('text.png') #.convert()
+
+text_rect = text.get_rect(centerx = screen.get_rect().centerx,
+                          y = 0.03*size[1])
+
+print(screen.get_rect().width)
+
+done = False
+while not done:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill((40, 35, 56))
+
+    screen.blit(text, text_rect )
+
+    pygame.display.update()
